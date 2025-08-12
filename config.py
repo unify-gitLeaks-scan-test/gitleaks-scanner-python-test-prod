afed35f0eec66b2e04216ac2906b4f4978e6643a
cbp-qa-automation <cbp-qa-automation@beescloud.com>
2025-08-12 19:49:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # 🚨 Hardcoded secrets (vulnerable)
 API_KEY = "bb3601f5-6574-4805-a910-f8e1e93ef083"
 DB_PASSWORD = "supersecret@123"
+SECRET_TOKEN=ghp_exampletoken1234567890787fdfdff
+PASSWORD="abcd@32433"
